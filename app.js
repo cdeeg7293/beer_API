8d3851eeee1a6a76daa49d44701f35a9a26f4113
cdeeg7293 <cdeeg1993@gmail.com>
2021-07-03 12:34:57 +0100

    added API call using fetch method

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://api.punkapi.com/v2/beers/random')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      const name = data[0].name
+      console.log(name)
+      const description = data[0].description
+      console.log(description)
+      const { volume } = data[0]
+      const volumeValue = volume.value
+      const volumeUnit = volume.unit
+    })
+})
